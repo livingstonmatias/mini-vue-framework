6e07356550f1dee79703be2c956f20d7d444b5b0
MAL <livingstonmatias@gmail.com>
2024-10-08 13:05:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'https://livingstonmatias.github.io/mini-framework/'
+  base: 'https://livingstonmatias.github.io/mini-vue-framework/'
 })
